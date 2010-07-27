ab9226f14c50781de0c9ac1e59bd8d6efbe36ed1
aguynamedryan <aguynamedryan+oss@gmail.com>
2010-07-27 10:51:05 -0700

    Rails 3.0.0rc broke the call to reset_javascript_include_default
    Changed to overwrite javascript_expansions[:defaults]

--- init.rb
+++ init.rb
@@ -1,6 +1,5 @@
 # uncomment to use jQuery.noConflict()
 #ActionView::Helpers::PrototypeHelper::JQUERY_VAR = 'jQuery'
 
-ActionView::Helpers::AssetTagHelper::JAVASCRIPT_DEFAULT_SOURCES = ['jquery','jquery-extensions','jquery-ui','jrails']
-ActionView::Helpers::AssetTagHelper::reset_javascript_include_default
+ActionView::Helpers::AssetTagHelper.javascript_expansions[:defaults] = ['jquery','jquery-extensions','jquery-ui','jrails']
 require 'jrails'
